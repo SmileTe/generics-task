f7bd98e2873464167652ccdf6a15600ea6cff167
grand <ferthlin@ya.ru>
2023-10-28 18:57:56 +0300

    JUnit

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,130 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Function;
+import java.util.Arrays;
+public class MyList<T extends Number> implements Iterable {
+    private T[] array;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
-  }
+    public MyList() {
+        array =(T[]) Array.newInstance(Number.class, DEFAULT_CAPACITY);;
+        size = 0;
+    }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    public MyList(T[] array) {
+        this.array = array;
+        size = array.length;
+    }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
-  }
+    @Override
+    public Iterator iterator() {
+        return new MyListIterator();
+    }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    public void add(T item) {
+        if (size == array.length) {
+            resize();
+        }
+        array[size++] = item;
+    }
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
-  }
+    public T get(int index) {
+        return array[index];
+    }
 
-  public int size() {
-    throw new RuntimeException("Not implemented");
-  }
+    private void resize() {
+        int newCapacity = array.length * 2;
+        array = Arrays.copyOf(array, newCapacity);
+    }
+
+    public T remove(int index) {
+        try {
+
+
+            T oldValue = array[index];
+            final int newSize;
+            fastRemove(array, index);
+
+            return oldValue;
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+           throw  new ArrayIndexOutOfBoundsException("Некорректный индекс");
+
+        }
+
+
+    }
+
+    private void fastRemove(Object[] es, int i) {
+        int newSize;
+        if ((newSize = size - 1) > i)
+            System.arraycopy(es, i + 1, es, i, newSize - i);
+        es[size = newSize] = null;
+    }
+
+    public <R extends Number> MyList<R> map(Function<T, R> mapper) {
+        MyList<R> list = new MyList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(mapper.apply(this.get(i)));
+        }
+        return list;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyList<?> myList = (MyList<?>) o;
+        return size == myList.size && Arrays.equals(array, myList.array);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        result = 31 * result + Arrays.hashCode(array);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MyList{" +
+                "array=" + Arrays.toString(array) +
+                ", size=" + size +
+                '}';
+    }
+
+    /*
+    статический класс может быть создан без экземпляра объекта
+    и может использоваться один объект статического класса для всех объектов класса MyList.
+    Но в данном случае идет обращение к данным объекта, поэтому не указан static
+     */
+
+    class MyListIterator implements Iterator<Integer>{
+       int nextIndex;
+        public MyListIterator() {
+            nextIndex = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextIndex<size;
+        }
+
+        @Override
+        public Integer next() {
+            Integer result =  (Integer) array[nextIndex];
+            nextIndex++;
+            return result;
+        }
+    }
+
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -1,11 +1,102 @@
 package org.example;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.Iterator;
 
 public class MyListTest {
-  @Test
-  public void test() {
-    Assertions.assertTrue(true);
-  }
-}
+    @Test
+    public void addSimpleTest() {
+        MyList test = new MyList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        Assertions.assertAll(
+                "3 сценария сравнения",
+                () -> Assertions.assertEquals(1, test.get(0)),
+                () -> Assertions.assertEquals(2, test.get(1)),
+                () -> Assertions.assertEquals(3, test.get(2))
+        );
+    }
+
+    @Test
+    public void addTest2() {
+        MyList test = new MyList<>();
+        test.add(1);
+        test.add(2);
+        test.add(2);
+        Assertions.assertAll(
+                "4 сценария сравнения",
+                () -> Assertions.assertEquals(1, test.get(0)),
+                () -> Assertions.assertEquals(2, test.get(1)),
+                () -> Assertions.assertEquals(2, test.get(2)),
+                () -> Assertions.assertEquals(3, test.size())
+        );
+    }
+
+    @Test
+    public void removeTest() {
+        MyList test = new MyList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.remove(1);
+        Assertions.assertAll(
+                "1 сценарий сравнения",
+                () -> Assertions.assertEquals(2, test.size()),
+                () -> Assertions.assertEquals(1, test.get(0)),
+                () -> Assertions.assertEquals(3, test.get(1))
+        );
+    }
+
+    @Test
+    public void removeTest_Exception() {
+        MyList test = new MyList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        Throwable exception = Assertions.assertThrows(ArrayIndexOutOfBoundsException.class,
+                ()->{test.remove(50); }
+                    );
+        Assertions.assertEquals("Некорректный индекс", exception.getMessage());
+    }
+
+    @Test
+    public void iteratorTest() {
+        MyList test = new MyList<>();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+
+        MyList test2 = new MyList<>();
+        Iterator<Integer> iterator = test.iterator();
+        while (iterator.hasNext()) {
+            test2.add(iterator.next());
+        }
+        Assertions.assertTrue(test.equals(test2));
+    }
+
+    @Test
+    public void equalsTest() {
+        Integer[] array = new Integer[]{1, 2, 3};
+
+        MyList test = new MyList<>(array);
+        MyList myList2 = new MyList<>(array);
+
+        Assertions.assertTrue(test.equals(myList2));
+    }
+
+    @Test
+    public void hashCodeTest() {
+
+        Integer[] array = new Integer[]{1,2,3};
+        MyList test =  new MyList<>(array);
+        MyList test2 =  new MyList<>(array);
+
+        Assertions.assertEquals(test.hashCode(), test2.hashCode());
+    }
+
+    }
+
+
